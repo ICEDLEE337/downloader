d9681b72488d7eb150e401673f745f97c4d5a0d8
icedlee337 <lee.e.norris@gmail.com>
2021-11-06 15:13:04 -0400

    fix: missing import

--- dl.js
+++ dl.js
@@ -1,15 +1,19 @@
 #! /usr/bin/env node
 
-const fs = require('fs');
+const { createWriteStream } = require('fs');
+const { resolve } = require('path');
 const ytdl = require('ytdl-core');
 const { argParser } = require('@onivoro/nodejs-cli-sdk');
+
 const { name, hash } = argParser(process.argv);
 const resolvedFolder = resolve(process.cwd())
 
 if (!hash) {
-  console.log("missing --hash blahblahblah'");
-  console.log("optionally add --name blahblahblah'");
+  console.log(`USEAGE:
+"dl --hash 'ghjys34sdf64f' --name 'my video'"
+`);  
   return;
 }
+
 ytdl(`https://www.youtube.com/watch?v=${hash}`)
-  .pipe(fs.createWriteStream(`${resolvedFolder}/${name}.mp4`));
+  .pipe(createWriteStream(`${resolvedFolder}/${name}.mp4`));

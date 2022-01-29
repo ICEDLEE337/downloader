862fafb170574f6e2b3c2f9ab10beb5b3dcfb44c
icedlee337 <lee.e.norris@gmail.com>
2022-01-29 00:11:08 -0500

    chore: tweaks

--- dl.js
+++ dl.js
@@ -5,17 +5,17 @@ const { resolve } = require('path');
 const ytdl = require('ytdl-core');
 const { argParser } = require('@onivoro/nodejs-cli-sdk');
 
-const { name, hash, filter } = argParser(process.argv);
+const { name, hash, filter, begin } = argParser(process.argv);
 const resolvedFolder = resolve(process.cwd())
 
 if (!hash) {
   console.log(`
 USEAGE:
 
-dl --hash 'ghjys34sdf64f' --name 'my video.mp3' --filter audioonly
+dl --hash 'ghjys34sdf64f' --name 'my video.mp3' --filter audioonly --begin '1:00'
 `);  
   return;
 }
 
-ytdl(`https://www.youtube.com/watch?v=${hash}`, {filter})
+ytdl(`https://www.youtube.com/watch?v=${hash.replace('https://www.youtube.com/watch?v=', '')}`, {filter , begin})
   .pipe(createWriteStream(`${resolvedFolder}/${name}`));
